9dc7679bef49a8b0d8dd80fca5c665126e10f448
snehasish-20 <snehasish.pati@sap.com>
2024-11-28 17:44:13 +0530

    authenticated the apis

--- src/main/java/com/dococ/ai/controller/AIController.java
+++ src/main/java/com/dococ/ai/controller/AIController.java
@@ -3,22 +3,34 @@ package com.dococ.ai.controller;
 import com.dococ.ai.dto.PromptDTO;
 import com.dococ.ai.dto.ResponseDTO;
 import com.dococ.ai.service.AIService;
+import com.dococ.ai.service.AuthenticationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/ai-service")
+@RequestMapping("/api/v1/ai")
 public class AIController {
 
     @Autowired
     AIService aiService;
 
+    @Autowired
+    AuthenticationService authenticationService;
+
     @PostMapping
-    ResponseDTO getRecommendation(@RequestBody PromptDTO promptDTO) throws JsonProcessingException {
-        return aiService.getSpecializationRecomendation(promptDTO);
+    ResponseEntity<ResponseDTO> getRecommendation(@RequestBody PromptDTO promptDTO, @RequestHeader String authorization) throws JsonProcessingException {
+        authenticationService.validateRole(authorization, List.of("PATIENT"));
+
+        List<String> availableSpecializations = aiService.getAvailableSpecializations(authorization);
+
+        return ResponseEntity.ok().body(aiService.getSpecializationRecomendation(promptDTO, availableSpecializations));
     }
 }
