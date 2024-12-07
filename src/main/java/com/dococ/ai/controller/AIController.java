77498906e887f24866b466f29d57a5430e28e38a
Paryank Namdeo <paryank.namdeo@sap.com>
2024-12-07 12:50:27 +0530

    cross origin

--- src/main/java/com/dococ/ai/controller/AIController.java
+++ src/main/java/com/dococ/ai/controller/AIController.java
@@ -7,16 +7,13 @@ import com.dococ.ai.service.AuthenticationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/ai")
+@CrossOrigin
 public class AIController {
 
     @Autowired
